08403fedeb66cec83e9afea593ac11b12f3c99b6
Paulo Matos <pmatos@linki.tools>
2017-09-17 14:15:30 +0200

    Add supporting shell scripts for testing.

---
+++ travis-checkconfig.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# This script, to be run by travis, reads lib/config.json and generates
+# dummy passwords file so that checkconfig does not complain.
+
+import os
+import json
+
+with open('lib/config.json') as conf:
+    data = json.load(conf)
+
+dummy_pass = 'TESTING_PASS'
+passwords = dict()
+
+for w in data['workers']:
+    name = w['name']
+    passwords[name] = dummy_pass
+
+if not os.path.exists('lib/passwords.json'):
+    with open('lib/passwords.json', 'w') as passfile:
+        json.dump(passwords, passfile)
+    print('Done.')
+else:
+    print('Nothing done. lib/passwords.json already exists')
